5c81799e436551965023597a7424647236576b59
Raph6n <naim.raphael@outlook.fr>
2024-04-09 06:52:07 +0000

    task 8 post order

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - fction to go through tree post-order
+ * @tree: root
+ * @func: pointer to func
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+if (tree == NULL && func == NULL)
+{
+}
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+
+func(tree->n);
+}
