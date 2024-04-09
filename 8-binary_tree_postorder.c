3d7095d6dc4f45c1e462b5370853e0f1f8b7ef14
Raph6n <naim.raphael@outlook.fr>
2024-04-09 07:58:57 +0000

    correct task 8 again

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,11 +9,15 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 
-  if (tree == NULL && func == NULL)
-    return;
+if (tree == NULL)
+{
+}
 
-  binary_tree_postorder(tree->left, func);
-  binary_tree_postorder(tree->right, func);
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
 
-  func(tree->n);
+if (func != NULL)
+{
+func(tree->n);
+}
 }
