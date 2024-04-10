7ce91b06173618c0c651c00101170a1a71bd898b
Raph6n <naim.raphael@outlook.fr>
2024-04-10 11:55:10 +0000

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-binary_tree_t *sibling = binary_tree_sibling(node;
+binary_tree_t *sibling = binary_tree_sibling(node);
 
 if (node == NULL && node->parent == NULL)
 return (NULL);
