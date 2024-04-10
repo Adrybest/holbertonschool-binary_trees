4e2b02f9db2ee4886ac9027e93896dbbb4c61631
Adrybest <adryanbestard@gmail.com>
2024-04-10 11:18:42 +0200

    modification of return value

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,14 +19,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	return (1);
 
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	return (binary_tree_is_full(tree));
+	return (binary_tree_is_full);
 }
