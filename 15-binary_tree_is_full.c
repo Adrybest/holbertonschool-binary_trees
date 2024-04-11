e686ae9d2570c82db3261343faf679a13a1bf350
Adrybest <adryanbestard@gmail.com>
2024-04-11 11:24:51 +0200

    add new value and return in the task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,21 +7,21 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if ((tree->left != NULL && tree->right == NULL) ||/*lookif left exist*/
-		(tree->left == NULL && tree->right != NULL) ||/*check if right exist*/
-		binary_tree_is_full(tree->left) == 0 ||/*look if left subtree is full*/
-		binary_tree_is_full(tree->right) == 0)/*look if right subtree is full*/
-		{
-			return (0);
-		}
-	}
-	return (1);
-
-	if (tree == NULL)
+		if (tree == NULL)
 	{
 		return (0);
 	}
-	return (binary_tree_is_full(tree));
+
+		if ((tree->left != NULL && tree->right == NULL) ||/*lookif left exist*/
+		(tree->left == NULL && tree->right != NULL))/*check if right exist*/
+		{
+			return (0);
+		}
+
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
